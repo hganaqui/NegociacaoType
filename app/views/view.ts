47ff1087b88168298667e01b3a8d827a96b34623
hganaqui <hganaqui@gmail.com>
2021-08-26 18:01:48 -0300

    TYpescript aula 3 parte 2

--- app/views/view.ts
+++ app/views/view.ts
@@ -1,9 +1,9 @@
+import { inspect } from "../decorators/inspect";
 import { logarTempoExecucao } from "../decorators/logar-tempo-de-execucao";
 
 export abstract class View<T> {
 
     protected elemento: HTMLElement;
-    private escapar = false;
 
     constructor(seletor: string, escapar?: boolean) {
         const elemento = document.querySelector(seletor);
@@ -12,18 +12,12 @@ export abstract class View<T> {
         } else {
             throw Error(`Seletor ${seletor} não existe no DOM. Verifique`);
         }
-        if (escapar) {
-            this.escapar = escapar;
-        }
     }
 
-    @logarTempoExecucao()
+    @logarTempoExecucao(true)
+    @inspect  
     public update(model: T): void {
         let template = this.template(model);
-        if (this.escapar) {
-            template = template
-                .replace(/<script>[\s\S]*?<\/script>/, '');
-        }
         this.elemento.innerHTML = template;
     }
 
