97eb4670e88bfffec8d7bacaedf375cce3e3cf1d
hganaqui <hganaqui@gmail.com>
2021-08-27 10:16:58 -0300

    Aula 3 parte 3 finalizado

--- app/decorators/dom-injector.ts
+++ app/decorators/dom-injector.ts
@@ -1,13 +1,19 @@
 export function domInject(seletor: string){
     return function(target:any, propertyKey: string)
     {
+        let elemento: HTMLElement;
         const getter = function(){
-            const elemento = document.querySelector(seletor);
+            if(!elemento)
+            {
+                elemento = <HTMLElement>document.querySelector(seletor);
+            }
+            
             return elemento;
         }
 
-        Object.defineProperty(target, propertyKey,{
-            get: getter
-        })
+        Object.defineProperty(
+        target, 
+        propertyKey,
+        {get: getter})
     }
 }
